53e4f77f61ace1d11960fb025314848eab4f53be
Anderson O. Aristides <anderson.aristides@fh.com.br>
2019-10-12 15:26:54 -0300

    Feat: Criado calculo de pintura

--- calculadora-pintura/main.py
+++ calculadora-pintura/main.py
@@ -2,5 +2,21 @@ largura: float = float(input('Qual a largura do cômodo? '))
 profundidade: float = float(input('Qual a profundidade do cômodo? '))
 altura: float = 2.9
 
-print("A área das paredes é: ", (2 * (largura + profundidade) * altura))
+area_paredes = (2 * (largura + profundidade) * altura)
 
+print(
+    'A área das paredes é: ',
+    area_paredes
+)
+
+area_teto: float = largura * profundidade
+
+print(
+    'A áre do teto é: ',
+    area_teto
+)
+
+print(
+    'A litragem de tinta necessária é: ',
+    (area_paredes + area_teto) / 10
+)
