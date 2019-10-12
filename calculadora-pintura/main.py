15c190d79789cdcf1910db1536fe983b99f298ca
Anderson O. Aristides <anderson.aristides@fh.com.br>
2019-10-12 15:22:40 -0300

    Feat: Criando calculadore de pintura

---
+++ calculadora-pintura/main.py
@@ -0,0 +1,6 @@
+largura: float = float(input('Qual a largura do cômodo? '))
+profundidade: float = float(input('Qual a profundidade do cômodo? '))
+altura: float = 2.9
+
+print("A área das paredes é: ", (2 * (largura + profundidade) * altura))
+
