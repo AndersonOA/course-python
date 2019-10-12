e3f76337985fc85fa81c12d808f940ef2ec191de
Anderson O. Aristides <anderson.aristides@fh.com.br>
2019-10-12 18:48:48 -0300

    Feat: Deixando variaveis locais visiveis dentro do escopo da classe apenas

--- calculadora-pintura/main.py
+++ calculadora-pintura/main.py
@@ -1,23 +1,26 @@
 from calculadora import Calculadora
+from comodo import Comodo
 
 calc = Calculadora()
 
-largura: float = float(input('Qual a largura do cômodo? '))
-profundidade: float = float(input('Qual a profundidade do cômodo? '))
-altura: float = 2.9
+comodo = Comodo(
+    input('Qual a largura do cômodo? '),
+    input('Qual a profundidade do cômodo? ')
+)
+
 
 # calc.area_paredes = (2 * (largura + profundidade) * altura)
 
 print(
     'A área das paredes é: ',
-    calc.calcular_area_paredes(largura, profundidade, altura)
+    calc.calcular_area_paredes(comodo)
 )
 
 # calc.area_teto: float = largura * profundidade
 
 print(
     'A áre do teto é: ',
-    calc.calcular_area_teto(largura, profundidade)
+    calc.calcular_area_teto(comodo)
 )
 
 print(
